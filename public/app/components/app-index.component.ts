5db98801b8ac59d806f74406ca83cfb1454f6d2f
rfprod <zoidenmacht@zoho.com>
2018-07-21 20:55:27 +0300

    (update) client: improved facebook page plugin initialization;
    optimized bandcamp images loading;
    
    removed junk from global styles.

--- public/app/components/app-index.component.ts
+++ public/app/components/app-index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ElementRef, ViewChild, Inject } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ElementRef, Inject, ViewChild, Renderer2 } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { MediaChange, ObservableMedia } from '@angular/flex-layout';
 
@@ -14,13 +14,14 @@ import { BandcampService } from '../services/bandcamp.service';
 		class: 'mat-body-1'
 	}
 })
-export class AppIndexComponent implements OnInit, OnDestroy {
+export class AppIndexComponent implements OnInit, AfterViewInit, OnDestroy {
 
 	/**
 	 * @param el Element reference
 	 * @param sanitizer DOM danitizer
 	 * @param media Observable media
 	 * @param emitter Event emitter service - components interaction
+	 * @param renderer Application renderer
 	 * @param firebaseService Service for making firebase requests
 	 * @param bandcampService Service for generating bandcamp urls
 	 * @param window Window reference
@@ -30,6 +31,7 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 		private sanitizer: DomSanitizer,
 		private media: ObservableMedia,
 		private emitter: EventEmitterService,
+		private renderer: Renderer2,
 		private firebaseService: FirebaseService,
 		private bandcampService: BandcampService,
 		@Inject('Window') private window: Window
@@ -64,6 +66,7 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 					this.bandcampAlbumsData[key] = value;
 				});
 				this.gridInitialized = true;
+				this.initFacebookJsSDK();
 				this.emitter.emitSpinnerStopEvent();
 				def.resolve();
 			})
@@ -80,6 +83,19 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 	 * Needeed for loading sequential iframes, one by one.
 	 */
 	public bcIframeCounter: number = 0;
+
+	/**
+	 * Indicates max bandcamp widgets count to render based on scroll position.
+	 */
+	public bcMaxCounter: number = 6;
+
+	/**
+	 * Resets bandcamp counters.
+	 */
+	private resetBandcampCounters(): void {
+		this.bcIframeCounter = 0;
+		this.bcMaxCounter = 6;
+	}
 
 	/**
 	 * Increments Bandcamp iframe counter.
@@ -120,6 +136,23 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 	public gridInitialized: boolean = false;
 
 	/**
+	 * Creates Facebook root div.
+	 * @return Facebook root div reference <div id="fb-root"></div>
+	 */
+	private createFbRoot(): any {
+		const doc: Document = this.window.document;
+		let ref: any = doc.getElementById('fb-root'); // try getting it first
+		if (!ref) {
+			// create 'fb-root' if it does not exist
+			ref = doc.createElement('div');
+			ref.id = 'fb-root';
+			const firstScriptTag: any = doc.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(ref, firstScriptTag);
+		}
+		return ref;
+	}
+
+	/**
 	 * Initializes facebook javascript sdk.
 	 *
 	 * see:
@@ -127,22 +160,60 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 	 * - https://blog.brunoscopelliti.com/facebook-authentication-in-your-angularjs-web-app/
 	 */
 	private initFacebookJsSDK(): void {
-		let js;
 		const id: string = 'facebook-jssdk';
 		const doc: Document = this.window.document;
-		const ref: any = doc.getElementsByTagName('script')[0];
-
+		const ref: any = this.createFbRoot();
+		console.log('ref', ref);
+		// return if script is already included
 		if (doc.getElementById(id)) {
 			return;
 		}
-
-		js = doc.createElement('script');
+		console.log('initFacebookJsSDK...');
+		const js: any = doc.createElement('script');
 		js.id = id;
 		js.async = true;
 		// js.src = "//connect.facebook.net/en_US/all.js";
-		js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=1771962743078907&autoLogAppEvents=1';
+		js.src = 'https://connect.facebook.net/en_US/sdk.js#status=1&xfbml=1&version=v3.0&appId=1771962743078907&channelUrl=channel.html';
 
 		ref.parentNode.insertBefore(js, ref);
+
+		console.log('this.window', this.window);
+	}
+
+	private removeFbJsSDK(): void {
+		const id: string = 'facebook-jssdk';
+		const doc: Document = this.window.document;
+		const ref: any = doc.getElementById('fb-root');
+		const js: any = doc.getElementById('facebook-jssdk');
+		// removed both script and fb-root
+		if (js) {
+			ref.parentNode.removeChild(js);
+			ref.parentNode.removeChild(ref);
+		}
+	}
+
+	@ViewChild('content') private content: ElementRef;
+
+	/**
+	 * Renderer2 listener instance.
+	 */
+	private rendererListener: any;
+	/**
+	 * Binds to mat-sidenav-content scroll event.
+	 */
+	private bindToContentScrollEvent(): void {
+		let previousScrollTopValue: number = 0;
+		this.rendererListener = this.renderer.listen(this.content.nativeElement.parentNode.parentNode, 'scroll', (event) => {
+			// console.log('mat-sidenav-content scroll, event', event);
+			const currentScrollTopValue: number = event.target.scrollTop;
+			// increment bandcamp max counter when user scrolls down
+			const currentBcMaxCounterValue: number = Math.ceil((currentScrollTopValue / event.target.clientHeight) * this.gridColumns * (this.gridColumns - 1));
+			if (previousScrollTopValue < currentScrollTopValue && this.bcMaxCounter < currentBcMaxCounterValue) {
+				this.bcMaxCounter = currentBcMaxCounterValue;
+				console.log('this.bcMaxCounter', this.bcMaxCounter);
+			}
+			previousScrollTopValue = currentScrollTopValue;
+		});
 	}
 
 	/**
@@ -151,7 +222,9 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 	public ngOnInit(): void {
 		console.log('ngOnInit: AppIndexComponent initialized');
 
-		this.initFacebookJsSDK();
+		this.bindToContentScrollEvent();
+
+		this.createFbRoot();
 
 		this.getBandcampAlbumsData().then(() => {
 			this.bandcampAlbumsDataKeys = Object.keys(this.bandcampAlbumsData);
@@ -169,6 +242,8 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 			if (event.mqAlias === 'xs' || event.mqAlias === 'sm') {
 				this.showBandcampGrid = false;
 				this.gridInitialized = false;
+				// reset bandcamp counters
+				this.resetBandcampCounters();
 			} else {
 				this.showBandcampGrid = true;
 				this.gridInitialized = true;
@@ -176,11 +251,15 @@ export class AppIndexComponent implements OnInit, OnDestroy {
 		});
 		this.subscriptions.push(sub);
 	}
+	public ngAfterViewInit(): void {
+		this.initFacebookJsSDK();
+	}
 	/**
 	 * Lifecycle hook called after component is destroyed.
 	 */
 	public ngOnDestroy(): void {
 		console.log('ngOnDestroy: AppIndexComponent destroyed');
+		this.removeFbJsSDK();
 		if (this.subscriptions.length) {
 			for (const sub of this.subscriptions) {
 				sub.unsubscribe();
